7cfb9e37e393680040517dc51b5949c643addadb
ali.abukahil <ali.abukahil@gmail.com>
2022-02-08 23:16:43 +0100

    Helpers and Configuration Files

---
+++ src/js/config.js
@@ -0,0 +1,7 @@
+// using uppercase API_URL because this is basically
+// a constant that will never change, using uppercase
+// for that kind of variable is kind of a common practice
+// especially in a configuration file like this
+
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes';
+export const TIMEOUT_SEC = 10;
