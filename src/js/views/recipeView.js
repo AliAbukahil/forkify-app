238dd8d1c90cb25f83d476f31aecb60fab28387d
ali.abukahil <ali.abukahil@gmail.com>
2022-02-13 17:13:58 +0100

    Error found in recipeView.js generateMarkup && was ._data protected properties

--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -24,11 +24,11 @@ class RecipeView extends View {
   _generateMarkup() {
     return `
             <figure class="recipe__fig">
-          <img src="${this.data.image}" alt="${
-      this.data.title
+          <img src="${this._data.image}" alt="${
+      this._data.title
     }" class="recipe__img" />
           <h1 class="recipe__title">
-            <span>${this.data.title}</span>
+            <span>${this._data.title}</span>
           </h1>
         </figure>
 
@@ -38,7 +38,7 @@ class RecipeView extends View {
               <use href="${icons}#icon-clock"></use>
             </svg>
             <span class="recipe__info-data recipe__info-data--minutes">${
-              this.data.cookingTime
+              this._data.cookingTime
             }</span>
             <span class="recipe__info-text">minutes</span>
           </div>
@@ -47,7 +47,7 @@ class RecipeView extends View {
               <use href="${icons}#icon-users"></use>
             </svg>
             <span class="recipe__info-data recipe__info-data--people">${
-              this.data.servings
+              this._data.servings
             }</span>
             <span class="recipe__info-text">servings</span>
 
@@ -77,7 +77,7 @@ class RecipeView extends View {
         <div class="recipe__ingredients">
           <h2 class="heading--2">Recipe ingredients</h2>
           <ul class="recipe__ingredient-list">
-            ${this.data.ingredients
+            ${this._data.ingredients
               .map(this._generateMarkupIngredient)
               .join('')}
 
@@ -88,13 +88,13 @@ class RecipeView extends View {
           <p class="recipe__directions-text">
             This recipe was carefully designed and tested by
             <span class="recipe__publisher">${
-              this.data.publisher
+              this._data.publisher
             }</span>. Please check out
             directions at their website.
           </p>
           <a
             class="btn--small recipe__btn"
-            href="${this.data.sourceUrl}"
+            href="${this._data.sourceUrl}"
             target="_blank"
           >
             <span>Directions</span>
@@ -109,21 +109,20 @@ class RecipeView extends View {
   // _Protected method naming convention
   _generateMarkupIngredient(ing) {
     return `
-      <li class="recipe__ingredient">
-        <svg class="recipe__icon">
-          <use href="${icons}#icon-check"></use>
-        </svg>
-        <div class="recipe__quantity">${
-          ing.quantity ? new Fraction(ing.quantity).toString() : ''
-        }</div>
-        <div class="recipe__description">
-          <span class="recipe__unit">${ing.unit}</span>
-          ${ing.description}
-        </div>
-      </li>
-        `;
+    <li class="recipe__ingredient">
+      <svg class="recipe__icon">
+        <use href="${icons}#icon-check"></use>
+      </svg>
+      <div class="recipe__quantity">${
+        ing.quantity ? new Fraction(ing.quantity).toString() : ''
+      }</div>
+      <div class="recipe__description">
+        <span class="recipe__unit">${ing.unit}</span>
+        ${ing.description}
+      </div>
+    </li>
+  `;
   }
 }
 
-// default exports
 export default new RecipeView();
