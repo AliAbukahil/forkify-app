64a326c3c7479b46b3c738f526635a995b9ebc48
ali.abukahil <ali.abukahil@gmail.com>
2022-02-18 11:07:52 +0100

    Developing a DOM Updating Algorithm

--- src/js/views/View.js
+++ src/js/views/View.js
@@ -20,6 +20,36 @@ export default class View {
     this._parentElement.insertAdjacentHTML('afterbegin', markup);
   }
 
+  update(data) {
+    this._data = data;
+    const newMarkup = this._generateMarkup();
+    console.log(newMarkup);
+
+    // This method will then convert that string into real DOM node objects
+    const newDOM = document.createRange().createContextualFragment(newMarkup);
+    const newElements = Array.from(newDOM.querySelectorAll('*'));
+    const curElements = Array.from(this._parentElement.querySelectorAll('*'));
+
+    newElements.forEach((newEl, i) => {
+      const curEl = curElements[i];
+      // console.log(curEl, newEl.isEqualNode(curEl));
+
+      // Update changed TEXT
+      if (
+        !newEl.isEqualNode(curEl) &&
+        newEl.firstChild?.nodeValue.trim() != ''
+      ) {
+        // console.log('😱', newEl.firstChild.nodeValue.trim());
+        curEl.textContent = newEl.textContent;
+      }
+      // Update changed ATTRIBUTES
+      if (!newEl.isEqualNode(curEl))
+        Array.from(newEl.attributes).forEach(attr =>
+          curEl.setAttribute(attr.name, attr.value)
+        );
+    });
+  }
+
   // _Protected method naming convention
   _clear() {
     this._parentElement.innerHTML = '';
